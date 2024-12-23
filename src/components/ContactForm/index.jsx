3ec32a0f1911c17b50f4e3988e6713ea34ab84f2
marcoslauxen <marcoslauxen075@gmail.com>
2024-12-23 15:37:32 -0400

    Primeiro commit do projeto MyContacts_Front-End

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,91 @@
+import PropTypes from "prop-types";
+
+import { useState } from "react";
+import { Form, ButtonContainer } from "./styles";
+
+import FormGroup from "../FormGroup";
+import Input from "../Input";
+import Select from "../Select";
+import Button from "../Button";
+
+export default function ContactForm({ buttonLabel }) {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+    const [category, setCategory] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    function handleNameChange(event) {
+        setName(event.target.value);
+
+        if (!event.target.value) {
+            setErrors((prevState) => [
+                ...prevState,
+                { field: 'name', message: 'Nome é obrigatório' },
+            ]);
+        } else {
+            setErrors((prevState) => prevState.filter(
+                (error) => error.field !== 'name',
+            ))
+        }
+    };
+
+    console.log(errors);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log({
+            name,
+            email,
+            phone,
+            category,
+        })
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormGroup>
+                <Input
+                    placeholder="Nome"
+                    value={name}
+                    onChange={handleNameChange}
+                />
+            </FormGroup>
+
+            <FormGroup>
+                <Input
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+            </FormGroup>
+
+            <FormGroup>
+                <Input
+                    placeholder="Telefone"
+                    value={phone}
+                    onChange={(event) => setPhone(event.target.value)}
+                />
+            </FormGroup>
+
+            <FormGroup>
+                <Select
+                    value={category}
+                    onChange={(event) => setCategory(event.target.value)}
+                >
+                    <option value="">Categoria</option>
+                    <option value="Instagram">Instagram</option>
+                    <option value="Discord">Discord</option>
+                </Select>
+            </FormGroup>
+
+            <ButtonContainer>
+                <Button type="submit">{buttonLabel}</Button>
+            </ButtonContainer>
+        </Form>
+    );
+}
+
+ContactForm.propTypes = {
+    buttonLabel: PropTypes.string.isRequired,
+};
